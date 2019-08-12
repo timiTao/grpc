4449530d4cb0daa75429af3829364c11868912ac
TimiTao <kunicki.tomasz@gmail.com>
2019-08-13 00:08:46 +0200

    working injecting data

--- packages/PHP/Test/docker/Producer/client.php
+++ packages/PHP/Test/docker/Producer/client.php
@@ -7,32 +7,54 @@ require getenv('VENDOR_AUTOLOAD');
 use TimiTao\GRPC\Contract\AddReply;
 use TimiTao\GRPC\Contract\AddRequest;
 use TimiTao\GRPC\Contract\CalculatorClient;
+use TimiTao\GRPC\Contract\SubtractReply;
+use TimiTao\GRPC\Contract\SubtractRequest;
 
 $server = getenv('CONSUMER_HOST');
 $port = getenv('CONSUMER_PORT');
+$inputFile = getenv('PRODUCER_INPUT_FILE');
+$outputFile = getenv('PRODUCER_OUTPUT_FILE');
 
 $client = new CalculatorClient(sprintf('%s:%s', $server, $port), [
     "credentials" => Grpc\ChannelCredentials::createInsecure(),
 ]);
 
-function testSync(CalculatorClient $client)
+function testAdd(CalculatorClient $client, int $x, int $y, int $expectedResult): bool
 {
-    $endLimit = 1000;
-    $start = microtime(true);
-    for ($i = 0; $i < $endLimit; ++$i) {
-        $add = new AddRequest();
-        $add->setX(1)->setY($i);
-
-        /** @var AddReply $response */
-        $response = $client->add($add)->wait()[0];
-        echo "Response: SUM " . $response->getSum(). "\n";
-        if ($i % 100 == 0) {
-            echo("Sent $i requests\n");
-        }
-    }
-    $end = microtime(true);
-    $time = ($end - $start) * 1000;
-    echo(sprintf("Took %d ms for %d synchronous calls\n", $time, $endLimit));
+    $request = (new AddRequest())->setX($x)->setY($y);
+    /** @var AddReply $response */
+    $response = $client->add($request)->wait()[0];
+    echo "Response: SUM " . $response->getSum() . "\n";
+    return $response->getSum() === $expectedResult;
 }
 
-testSync($client);
+function testSub(CalculatorClient $client, int $x, int $y, int $expectedResult): bool
+{
+    $request = (new SubtractRequest())->setX($x)->setY($y);
+    /** @var SubtractReply $response */
+    $response = $client->subtract($request)->wait()[0];
+    echo "Response: SUB " . $response->getDiff() . "\n";
+    return $response->getDiff() === $expectedResult;
+}
+
+$data = require $inputFile;
+$endLimit = 1000;
+$start = microtime(true);
+foreach ($data as $key => $row) {
+    switch ($row['ACTION']) {
+        case 'ADD':
+            $response = testAdd($client, $row['X'], $row['Y'], $row['EXPECTED']);
+            break;
+        case 'SUB':
+            $response = testSub($client, $row['X'], $row['Y'], $row['EXPECTED']);
+            break;
+        default:
+            throw new RuntimeException('Unknown request data: ' . print_r($row['ACTION'], true));
+    }
+    $data[$key]['RESULT'] = $response;
+}
+$end = microtime(true);
+$time = ($end - $start) * 1000;
+echo(sprintf("Took %d ms for %d synchronous calls\n", $time, $endLimit));
+
+file_put_contents($outputFile, sprintf("<?php \n return %s; \n", var_export($data, true)));
---
+++ packages/PHP/Test/docker/Producer/data.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+    [
+        'ACTION' => 'ADD',
+        'X' => 2,
+        'Y' => 5,
+        'EXPECTED' => 7
+    ],
+    [
+        'ACTION' => 'SUB',
+        'X' => 10,
+        'Y' => 7,
+        'EXPECTED' => 3
+    ],
+
+    [
+        'ACTION' => 'ADD',
+        'X' => 20,
+        'Y' => 5,
+        'EXPECTED' => 7
+    ],
+];
